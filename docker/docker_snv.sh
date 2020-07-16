683d43ace3623e939d00b5ba16c4e76609db5a4c
马过 <mmmaaaggg@163.com>
2020-07-16 13:26:52 +0800

    Update docker_snv.sh

--- docker/docker_snv.sh
+++ docker/docker_snv.sh
@@ -1,8 +1,8 @@
 docker run -v /usr/local/svn:/home/svn 
            -v /usr/local/svn/passwd:/etc/subversion/passwd 
-		       -v /usr/local/apache2:/run/apache2 
-		       --name redm_axure
-		       -p 3380:13380 
-		       -p 3690:13960 
-		       -e SVN_REPONAME=repos 
-		       -d docker.io/elleflorio/svn-server
+           -v /usr/local/apache2:/run/apache2 
+           --name redm_axure
+           -p 3380:13380 
+           -p 3690:13960 
+           -e SVN_REPONAME=repos 
+           -d docker.io/elleflorio/svn-server
