bb9bfeb577b0fc4cf6a5f17b60e704733c3eb0a5
马过 <mmmaaaggg@163.com>
2020-09-17 09:06:10 +0800

    Update docker_gitlab.bat

--- docker/docker_gitlab.bat
+++ docker/docker_gitlab.bat
@@ -1,9 +1,9 @@
 docker run -d --hostname localhost \
-    -p 10080:80 \
+    -p 8888:80 \
     -p 10443:443 \
+    -p 10022:22 \
     --name gitlab \
-    -v /var/gitlab/docker.sock:/var/run/docker.sock \
     -v /var/gitlab/etc:/etc/gitlab \
-    -v /var/gitlab/data:/var/data/gitlab \
+    -v /var/gitlab/data:/var/opt/gitlab \
     -v /var/gitlab/log:/var/log/gitlab \
     gitlab/gitlab-ce:latest
