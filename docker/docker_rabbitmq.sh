1fc505b3ab404739a4a320ccd566b187ab498d8a
mmmaaaggg <mmmaaaggg@163.com>
2020-12-05 17:12:11 +0800

    fix: management

--- docker/docker_rabbitmq.sh
+++ docker/docker_rabbitmq.sh
@@ -1,3 +1,3 @@
 set -x
 docker pull rabbitmq:management
-docker run -dit --name rabbitmq_mg -e RABBITMQ_DEFAULT_USER=admin -e RABBITMQ_DEFAULT_PASS=Abcd1234 -p 15672:15672 -p 5672:5672 rabbitmq:managemen
+docker run -dit --name rabbitmq_mg -e RABBITMQ_DEFAULT_USER=admin -e RABBITMQ_DEFAULT_PASS=Abcd1234 -p 15672:15672 -p 5672:5672 rabbitmq:management
